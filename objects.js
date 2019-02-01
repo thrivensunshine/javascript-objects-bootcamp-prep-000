e802036b57175ce13a4fd9a4e7dad40eafac776b
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-01 20:45:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,3 +8,10 @@ function updatePlaylist(obj, name, song){
   return obj
 }
  
+  
+function removeFromPlaylist(obj,name){
+ delete obj[name]
+ return obj
+  // delete obj['name']
+  // return obj
+}
